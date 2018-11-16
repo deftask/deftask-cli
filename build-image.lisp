52cf89898ed1777dfd331f1ab1d976aa4aafeb49
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-16 18:32:48 +0530

    Compress the image

--- build-image.lisp
+++ build-image.lisp
@@ -7,4 +7,5 @@
   (sb-ext:save-lisp-and-die "deftask-cli"
                             :executable t
                             :save-runtime-options t
+                            :compression t
                             :toplevel #'deftask-cli:main))
