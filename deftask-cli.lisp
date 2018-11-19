c3b4d3f75b37c447ade8054217bbfce0a0442383
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-20 00:32:46 +0530

    Fix indentation

--- deftask-cli.lisp
+++ deftask-cli.lisp
@@ -17,18 +17,18 @@
 
   (dolist (c safe-read-from-string-blacklist)
     (set-macro-character
-      c #'safe-reader-error nil rt))
+     c #'safe-reader-error nil rt))
 
   (defun safe-read-from-string (s &optional fail)
     (if (stringp s)
-      (let ((*readtable* rt) *read-eval*)
-        (handler-bind
-          ((error (lambda (condition)
-                    (declare (ignore condition))
-                    (return-from
-                      safe-read-from-string fail))))
-          (read-from-string s)))
-      fail)))
+        (let ((*readtable* rt) *read-eval*)
+          (handler-bind
+              ((error (lambda (condition)
+                        (declare (ignore condition))
+                        (return-from
+                         safe-read-from-string fail))))
+            (read-from-string s)))
+        fail)))
 
 ;;; utils
 
