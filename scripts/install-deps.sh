d09ea93cbb1883fbe761c2a45a8a8cb5c5778307
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-23 03:26:21 +0530

    Fix termcolor dependency

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,3 +3,6 @@
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 
 sbcl --disable-debugger --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(setf ql-util::*do-not-prompt* t)' --eval '(ql:add-to-init-file)' --quit
+
+cd ~/quicklisp/local-projects/
+git clone https://github.com/chaitanyagupta/termcolor.git
