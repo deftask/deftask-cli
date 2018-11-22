8909cb61a26db1e7b0648139bd51c21e834309dc
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-23 03:44:22 +0530

    Fix build for linux

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ "$TRAVIS_OS_NAME" == "sbcl" ]]; then
+    export SB_DIST=sbcl-1.4.13-x86-64-linux
+    export TARBALL=$SB_DIST-linux-binary.tar.bz2
+    wget http://prdownloads.sourceforge.net/sbcl/$TARBALL
+    tar -xjvf $TARBALL
+    cd $SB_DIST
+    sudo sh install.sh
+    cd -
+fi
+
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 
 sbcl --disable-debugger --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(setf ql-util::*do-not-prompt* t)' --eval '(ql:add-to-init-file)' --quit
