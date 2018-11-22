4c24b3b8aa002c1edad202300d1c1c52a22d4176
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-23 03:11:13 +0530

    Fix build scripts

--- scripts/build-image.lisp
+++ scripts/build-image.lisp
@@ -1,3 +1,4 @@
+(push *default-pathname-defaults* asdf:*central-registry*)
 (ql:quickload "cl-fad")
 (ql:quickload "deftask-cli")
 
@@ -6,5 +7,6 @@
     (assert os nil "No OS given")
     (ensure-directories-exist (format nil "build/~A/" os))))
 
-(cl-fad:copy-file "README.md" (make-pathname :name "README.md" :defaults *build-dir*))
+(cl-fad:copy-file "README.md" (make-pathname :name "README.md" :defaults *build-dir*) :overwrite t)
+
 (deftask-cli:build-image (make-pathname :name "deftask" :defaults *build-dir*))
