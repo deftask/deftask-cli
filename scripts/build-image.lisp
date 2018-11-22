0685130a627b6804a6f323ead50166943cdb8d32
Chaitanya Gupta <mail@chaitanyagupta.com>
2018-11-23 04:31:57 +0530

    Add dist creation script

--- scripts/build-image.lisp
+++ scripts/build-image.lisp
@@ -1,13 +1,11 @@
 (push *default-pathname-defaults* asdf:*central-registry*)
-(ql:quickload "cl-fad")
 (ql:quickload "cffi-grovel") ; https://github.com/quicklisp/quicklisp-client/issues/108
 (ql:quickload "deftask-cli")
 
 (defvar *build-dir*
-  (let ((os (deftask-cli::getenv "TRAVIS_OS_NAME")))
+  (let ((os (or (deftask-cli::getenv "TRAVIS_OS_NAME")
+                (string-downcase (uiop/os:operating-system)))))
     (assert os nil "No OS given")
     (ensure-directories-exist (format nil "build/~A/" os))))
-
-(cl-fad:copy-file "README.md" (make-pathname :name "README.md" :defaults *build-dir*) :overwrite t)
 
 (deftask-cli:build-image (make-pathname :name "deftask" :defaults *build-dir*))
---
+++ scripts/create-dist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OS=$TRAVIS_OS_NAME
+cd build/$OS
+
+VERSION=$(./deftask version)
+TARBALL=deftask-cli-$OS-v$VERSION.tar.gz
+
+ln ../../README.md
+tar czvf $TARBALL *
+mkdir -p ../dists
+cp $TARBALL ../dists/
